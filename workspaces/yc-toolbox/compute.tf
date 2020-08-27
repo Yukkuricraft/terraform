afbc3cbe21203b9bf37a1b925db7861014f607da
Yuto Takamoto <scarletjaeger@gmail.com>
2020-08-27 04:06:22 +0000

    Add YC-Sandbox instance definition.

--- workspaces/yc-toolbox/compute.tf
+++ workspaces/yc-toolbox/compute.tf
@@ -8,6 +8,9 @@ data "template_file" "cloud-init" {
   }
 }
 
+### Toolbox
+
+# Toolbox is a simple COS instance running various docker images as tooling.
 resource "google_compute_instance" "yc-toolbox" {
   name         = "yc-toolbox"
   machine_type = "f1-micro"
@@ -42,5 +45,42 @@ resource "google_compute_instance" "yc-toolbox" {
 
   metadata = {
     user-data = "${data.template_file.cloud-init.rendered}"
+  }
+}
+
+### Sandbox
+
+# Sandbox is the host containing a "dev YC" environment for non-sysadmins to practice and learn linux
+# Can probably make these into modules? TODO.
+resource "google_compute_instance" "yc-sandbox" {
+  name         = "yc-sandbox"
+  machine_type = "n1-highcpu-2"
+  zone         = "us-central1-a"
+  project      = google_project.yc-toolbox.project_id
+
+  tags = ["allow-ssh-2222"]
+
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = module.secrets.gcp_custom_yc_toolbox_image_uri
+      type  = "pd-standard"
+      size  = "50"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+      // Static IP in the future depending??
+    }
+  }
+
+  service_account {
+    email  = google_service_account.yc-infra-bot.email
+    scopes = ["cloud-platform"]
   }
 }
